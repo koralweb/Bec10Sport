82de98166e6ef04702e44730f4d0659267c9073a
35235225 <a35235225@Mac-mini-35235225.local>
2024-04-05 09:38:11 +0300

    add some products

--- src/data/products.js
+++ src/data/products.js
@@ -6,6 +6,20 @@ const products = [
     price: 100,
     type: 'first',
   },
+  {
+    name: 'Product name 2',
+    image: 'require(путь к продукту)',
+    desc: 'Product description',
+    price: 100,
+    type: 'salads',
+  },
+  {
+    name: 'Product name 2',
+    image: 'require(путь к продукту)',
+    desc: 'Product description',
+    price: 100,
+    type: 'dessert',
+  },
 ];
 
 export default products;
