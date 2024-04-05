621c10120c2e3c980f1d7c6eac04508fbb84ac9e
koralweb <dev@koralweb.com>
2024-04-05 09:36:04 +0300

    create products data

---
+++ src/data/products.js
@@ -0,0 +1,11 @@
+const products = [
+  {
+    name: 'Product name',
+    image: 'require(путь к продукту)',
+    desc: 'Product description',
+    price: 100,
+    type: 'first',
+  },
+];
+
+export default products;
